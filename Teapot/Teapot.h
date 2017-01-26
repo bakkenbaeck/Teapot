d0548934a6d7059c7e6c995cc11eb1fb213d98f4
Igor Ranieri Elland <igor@elland.me>
2017-01-26 16:36:09 +0100

    Initial setup.

---
+++ Teapot/Teapot.h
@@ -0,0 +1,19 @@
+//
+//  Teapot.h
+//  Teapot
+//
+//  Created by Igor Ranieri on 26/01/2017.
+//  Copyright © 2017 B&B. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Teapot.
+FOUNDATION_EXPORT double TeapotVersionNumber;
+
+//! Project version string for Teapot.
+FOUNDATION_EXPORT const unsigned char TeapotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Teapot/PublicHeader.h>
+
+
