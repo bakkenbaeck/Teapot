ccd5822a3d45200a2f7a84b4b485494623ca8482
marijnschilling <marijnschilling@gmail.com>
2017-08-24 14:18:30 +0200

    Mocked errors (#8)
    * Write the test for unauthorized error
    
    * Add statuscode to mocked teapot
    
    * Add some documentation 📝
    
    * Return the statuscode!
    
    * Initialise the MockTeapot with (defaulted) statusCode
    
    * Add status code to documentation

--- Teapot/MockTeapot.swift
+++ Teapot/MockTeapot.swift
@@ -3,29 +3,50 @@ import Foundation
 /// A subclass of Teapot to be used for mocking
 open class MockTeapot: Teapot {
 
-    open var currentBundle: Bundle
+    private var currentBundle: Bundle
 
+    /// The status codes in words to be set as status code
+    public enum StatusCode: Int {
+        case ok = 200
+        case created = 201
+        case unauthorized = 401
+        case forbidden = 403
+        case notFound = 404
+        case internalServerError = 500
+        case serviceUnavailable = 503
+    }
+
+    /// Errors specific to parsing the specified mock file
     public enum MockError: Error {
         case missingMockFile(String)
         case invalidMockFile(String)
     }
-    
+
+    /// The status code the URL request should return
+    private let statusCode: StatusCode
+
     /// Initialiser.
     ///
     /// - Parameters:
     ///   - bundle: the bundle of your test target. When you add a json file with the name of the endpoint to your test target it will return this data.
-    public init(bundle: Bundle) {
+    ///   - statusCode: the status code for the response to return errors. Default is 200 "ok" 👌
+    public init(bundle: Bundle, statusCode: StatusCode = .ok) {
         self.currentBundle = bundle
+        self.statusCode = statusCode
 
         super.init(baseURL: URL(string: "https://mock.base.url.com")!)
     }
 
     override func execute(verb _: Verb, path: String, parameters _: RequestParameter? = nil, headerFields _: [String: String]? = nil, timeoutInterval _: TimeInterval = 5.0, allowsCellular _: Bool = true, completion: @escaping ((NetworkResult) -> Void)) {
         self.getMockedData(forPath: path) { json, error in
-            let response = HTTPURLResponse(url: URL(string: path)!, statusCode: 200, httpVersion: nil, headerFields: nil)
+            var mockedError = error
+            let response = HTTPURLResponse(url: URL(string: path)!, statusCode: self.statusCode.rawValue, httpVersion: nil, headerFields: nil)
             let requestParameter = json != nil ? RequestParameter(json!) : nil
 
-            let networkResult = NetworkResult(requestParameter, response!, error)
+            if self.statusCode != .ok {
+                mockedError = TeapotError.invalidResponseStatus
+            }
+            let networkResult = NetworkResult(requestParameter, response!, mockedError)
 
             completion(networkResult)
         }
