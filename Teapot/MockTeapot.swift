1813d9314c21301eefda50ff15ca5b18e88906f9
marijnschilling <marijnschilling@gmail.com>
2017-08-30 15:21:12 +0200

    Specify mock file name (#10)
    * Add state and test for no content
    
    * Get the source from the specified mock file name
    
    * Init with the file name
    
    * Make it possible to override centain endpoints
    
    * Make method public and document it
    
    * Implement feedback!
    
    * Add self

--- Teapot/MockTeapot.swift
+++ Teapot/MockTeapot.swift
@@ -2,12 +2,10 @@ import Foundation
 
 /// A subclass of Teapot to be used for mocking
 open class MockTeapot: Teapot {
-
-    private var currentBundle: Bundle
-
     /// The status codes in words to be set as status code
     public enum StatusCode: Int {
         case ok = 200
+        case noContent = 204
         case created = 201
         case unauthorized = 401
         case forbidden = 403
@@ -22,19 +20,35 @@ open class MockTeapot: Teapot {
         case invalidMockFile(String)
     }
 
-    /// The status code the URL request should return
+    private let currentBundle: Bundle
+    private let mockFilename: String
     private let statusCode: StatusCode
+
+    private var endpointsToOverride = [String: String]()
 
     /// Initialiser.
     ///
     /// - Parameters:
-    ///   - bundle: the bundle of your test target. When you add a json file with the name of the endpoint to your test target it will return this data.
+    ///   - bundle: the bundle of your test target, where it will search for the mock file
+    ///   - mockFileName: the name of the mock file containing the json that will be returned
     ///   - statusCode: the status code for the response to return errors. Default is 200 "ok" 👌
-    public init(bundle: Bundle, statusCode: StatusCode = .ok) {
+    public init(bundle: Bundle, mockFilename: String, statusCode: StatusCode = .ok) {
         self.currentBundle = bundle
+        self.mockFilename = mockFilename
         self.statusCode = statusCode
 
         super.init(baseURL: URL(string: "https://mock.base.url.com")!)
+    }
+
+    /// overrideEndPoint.
+    /// set the filename of the mocked json you want to return for a call to a certain endpoint
+    /// for example when you have a security call to the server that get's called every time you do an APICall
+    ///
+    /// - Parameters:
+    ///   - endPoint: the endpoint that needs to get overridden
+    ///   - filename: the name of the json file from which you want the data to be returned
+    public func overrideEndPoint(_ endPoint: String, withFilename filename: String) {
+        self.endpointsToOverride[endPoint] = filename
     }
 
     override func execute(verb _: Verb, path: String, parameters _: RequestParameter? = nil, headerFields _: [String: String]? = nil, timeoutInterval _: TimeInterval = 5.0, allowsCellular _: Bool = true, completion: @escaping ((NetworkResult) -> Void)) {
@@ -43,9 +57,10 @@ open class MockTeapot: Teapot {
             let response = HTTPURLResponse(url: URL(string: path)!, statusCode: self.statusCode.rawValue, httpVersion: nil, headerFields: nil)
             let requestParameter = json != nil ? RequestParameter(json!) : nil
 
-            if self.statusCode != .ok {
+            if self.statusCode.rawValue >= 300 {
                 mockedError = TeapotError.invalidResponseStatus
             }
+
             let networkResult = NetworkResult(requestParameter, response!, mockedError)
 
             completion(networkResult)
@@ -53,7 +68,8 @@ open class MockTeapot: Teapot {
     }
 
     func getMockedData(forPath path: String, completion: @escaping (([String: Any]?, Error?) -> Void)) {
-        let resource = (path as NSString).lastPathComponent
+        let endPoint = (path as NSString).lastPathComponent
+        let resource = self.endpointsToOverride[endPoint] ?? self.mockFilename
 
         if let url = currentBundle.url(forResource: resource, withExtension: "json") {
             do {
--- TeapotTests/MockTests.swift
+++ TeapotTests/MockTests.swift
@@ -4,10 +4,11 @@ import XCTest
 class MockTests: XCTestCase {
 
     func testMock() {
-        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self))
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "get")
+
         mockedTeapot.get("/get") { (result: NetworkResult) in
             switch result {            
-            case .success(let json, let response):
+            case .success(let json, _):
                 XCTAssertEqual(json!.dictionary!["key"] as! String, "value")
             case .failure:
                 XCTFail()
@@ -16,7 +17,8 @@ class MockTests: XCTestCase {
     }
 
     func testMissingMock() {
-        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self))
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "missing")
+
         mockedTeapot.get("/missing") { (result: NetworkResult) in
             switch result {
             case .success:
@@ -33,7 +35,8 @@ class MockTests: XCTestCase {
     }
 
     func testInvalidMock() {
-        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self))
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "invalid")
+
         mockedTeapot.get("/invalid") { (result: NetworkResult) in
             switch result {
             case .success:
@@ -49,14 +52,42 @@ class MockTests: XCTestCase {
         }
     }
 
+    func testNoContent() {
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "get", statusCode: .noContent)
+        mockedTeapot.get("/get") { (result: NetworkResult) in
+            switch result {
+            case .success(_, let response):
+                XCTAssertEqual(response.statusCode, 204)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+
     func testUnauthorizedError() {
-        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), statusCode: .unauthorized)
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "get", statusCode: .unauthorized)
+        
         mockedTeapot.get("/get") { (result: NetworkResult) in
             switch result {
             case .success:
                 XCTFail()
             case .failure(_, let response, _):
                 XCTAssertEqual(response.statusCode, 401)
+            }
+        }
+    }
+
+    func testEndPointOverriding() {
+        let mockedTeapot = MockTeapot(bundle: Bundle(for: MockTests.self), mockFilename: "get")
+        mockedTeapot.overrideEndPoint("overridden", withFilename: "overridden")
+
+        mockedTeapot.get("/overridden") { (result: NetworkResult) in
+            switch result {
+            case .success(let json, _):
+                XCTAssertEqual(json!.dictionary!["overridden"] as! String, "value")
+            case .failure(let error):
+                print(error)
+                XCTFail()
             }
         }
     }
