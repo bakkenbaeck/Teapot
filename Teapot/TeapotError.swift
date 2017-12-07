2c9d109d7c6444d7d450ba22770a18d25a44d295
marijnschilling <marijnschilling@gmail.com>
2017-12-07 12:48:40 +0100

    Don't make the initialiser public

--- Teapot/TeapotError.swift
+++ Teapot/TeapotError.swift
@@ -47,7 +47,7 @@ public struct TeapotError: Error, CustomStringConvertible {
 
     public var description: String
 
-    public init(withType type: ErrorType, description: String, responseStatus: Int? = nil, underlyingError: Error? = nil) {
+    init(withType type: ErrorType, description: String, responseStatus: Int? = nil, underlyingError: Error? = nil) {
         
         self.type = type
         self.description = description
